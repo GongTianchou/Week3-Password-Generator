1272ce6e65aa6cb8452bcb676277c42b2fceb72a
Tianchou Gong <tianchougong@gmail.com>
2022-02-27 23:56:39 -0500

    JS uploaded

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +9,36 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword(){
+  var passwordLength = 0;
+  var password = "";
+  while( passwordLength <8 || passwordLength >128){
+  passwordLength = prompt("Please enter the password length between 8 and 128");
+  }
+}
+  var validPasswordType = false;
+  var numeric = true;
+  var upperLetter = true;
+  var lowerLetter = ture;
 
+  while(!validPasswordType){
+    numeric = alert("Any Numberic?");
+    upperLetter = alert("Any Upperletter");
+    lowerLetter = alert("Any Lowerletter?")
+
+    if(numeric || upperLetter || lowerLetter){
+      validPasswordType = ture;
+    }else{
+      alert("Please select at least one type of passowrd");
+    }
+  }
+   var numericList = "0123456789"
+   var upperList = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+   var lowerList = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+   var PasswordType = "";
+
+   
+   
+   
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
